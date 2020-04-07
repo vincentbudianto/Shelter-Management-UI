9a7648b618ddfcb1f4d6c539c3a03cd05d536b13
ardyh <13517062@std.stei.itb.ac.id>
2020-04-07 14:30:06 +0700

    integrated functionalities for disaster dashboard

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -14,8 +14,9 @@
             v-model="selectedShelterDisasterName"
           ></v-select>
         </v-row>
+        <v-btn v-on:click="btnClickLihatBencana">Lihat Bencana</v-btn>
       </v-col>
-      <v-col align="center">
+      <v-col align="center" v-if="selectedShelterDisasterName">
         <v-row>
           <h2>Pilih Posko</h2>  
         </v-row>
@@ -24,9 +25,9 @@
             :items="shelterNames"
             v-model="selectedShelterName"
           ></v-select>
-        </v-row>
+        </v-row>  
+        <v-btn v-on:click="btnClickLihatPosko">Lihat Posko</v-btn>
       </v-col>
-      <v-btn v-on:click="btnClickLihat">Lihat</v-btn>
 
       <!-- Map Section-->
       <div>
@@ -37,62 +38,44 @@
             <vl-source-osm></vl-source-osm>
           </vl-layer-tile>
 
-          <vl-layer-vector>
-            <vl-feature>
+            <!-- Render points and information per disaster -->
+              <vl-feature v-for="coordinate in renderedCoordinates" v-bind:key="coordinate.DisasterID">
+                <vl-geom-point
+                  :coordinates="[coordinate.Longitude, coordinate.Latitude]"
+                ></vl-geom-point>
+                <vl-style-box>
+                  <vl-style-circle :radius="20">
+                    <vl-style-fill color="red"></vl-style-fill>
+                    <vl-style-stroke color="white"></vl-style-stroke>
+                  </vl-style-circle>
+                </vl-style-box>
 
-              <vl-geom-point
-                :coordinates="[12.492442,41.890170]"
-              ></vl-geom-point>
-              <!-- <vl-style-box>
-                <vl-style-circle :radius="20">
-                  <vl-style-fill color="white"></vl-style-fill>
-                  <vl-style-stroke color="red"></vl-style-stroke>
-                </vl-style-circle>
-              </vl-style-box> -->
+                <vl-overlay :position="[coordinate.Longitude, coordinate.Latitude]">
+                  <div class="overlay-content">
+                    {{disastersData[coordinate.DisasterID - 1].Name}}
+                  </div>
+                </vl-overlay>
+              </vl-feature>
 
-              <vl-geom-point
-                :coordinates="[-1,2]"
-              ></vl-geom-point>
-              <vl-style-box>
-                <vl-style-circle :radius="20">
-                  <vl-style-fill color="red"></vl-style-fill>
-                  <vl-style-stroke color="white"></vl-style-stroke>
-                </vl-style-circle>
-              </vl-style-box>
-
-            </vl-feature>
-
-            <vl-feature>
-
-              <vl-geom-point
-                :coordinates="[12.492442,41.890170]"
-              ></vl-geom-point>
-              <!-- <vl-style-box>
-                <vl-style-circle :radius="20">
-                  <vl-style-fill color="white"></vl-style-fill>
-                  <vl-style-stroke color="red"></vl-style-stroke>
-                </vl-style-circle>
-              </vl-style-box> -->
-
-              <!-- <vl-geom-point
-                :coordinates="[-1,2]"
-              ></vl-geom-point> -->
-              <vl-style-box>
-                <vl-style-circle :radius="20">
-                  <vl-style-fill color="red"></vl-style-fill>
-                  <vl-style-stroke color="white"></vl-style-stroke>
-                </vl-style-circle>
-              </vl-style-box>
-
-            </vl-feature>
-          </vl-layer-vector>
         </vl-map>
       </div>
 
-
-      <canvas id="victim-by-gender"></canvas>
+      <!-- chart section -->
+      <div>
+        <div v-if="selectedShelterDisasterName">
+          <a>{{selectedShelterDisasterName}}</a>
+          <a>Skala:{{selectedShelterDisasterScale}}</a>
+        </div>
+        <div>
+          <a>Jumlah Korban</a>
+          <a>{{countVictimInCurrentScope}}</a>
+          <a>orang</a>
+          <a>{{currentDashboardScope}}</a>
+        </div>
+      </div>
+      <!-- <canvas id="victim-by-gender"></canvas> -->
       <canvas id="victim-by-age"></canvas>
-      <canvas id="victim-by-condition"></canvas>
+      <!-- <canvas id="victim-by-condition"></canvas> -->
     </div>
   </div>
 </template>
@@ -110,12 +93,18 @@
     margin: auto;
     background-color: white;
   }
+  .overlay-content{
+  background-color:#FFFFFF;
+  padding: 10px;
+  width:auto;
+  height: auto;
+}
 </style>
 
 <script>
   import axios from 'axios';
 
-  var victimTemplateChart = {
+  const victimTemplateChart = {
     type: 'horizontalBar',
     data: {
       labels: [],
@@ -146,50 +135,75 @@
       }
     }
   }
-
-  const victimData = {
-    Gender: {
-      title: "Gender",
-      label: ["Laki-laki", "Perempuan"],
-      count: [20, 40]
-    },
-    Age: {
-      title: "Age",
-      label: ["<10", "10-20", "20-30", "30-40", "40-50", "50-60", "60-70", ">70"],
-      count: [10, 20, 40, 15, 20, 30, 10, 5]
-    },
-    Condition: {
-      title: "Condition",
-      label: ["Hidup", "Meninggal"],
-      count: [55, 30]
-    }
-  } 
+  
+  // victimData = {
+  //   Gender: {
+  //     title: "Gender",
+  //     label: ["Laki-laki", "Perempuan"],
+  //     count: [20, 40]
+  //   },
+  //   Age: {
+  //     title: "Age",
+  //     label: ["<10", "10-19", "20-39", "40-59", "60-79", ">79"],
+  //     count: [10, 20, 40, 15, 20, 30, 10, 5]
+  //   },
+  //   Condition: {
+  //     title: "Condition",
+  //     label: ["Hidup", "Meninggal"],
+  //     count: [55, 30]
+  //   }
+  // } 
 
 
 export default {
 
   data () {
     return {
-      victimTemplateChart,
-      victimData,
 
       //general data
       dashboardData: [],
       disastersData: [],
+      renderedDashboardData: [],
 
       //selection section
       shelterList: [],
       shelterDisasterNames: [],
       selectedShelterDisasterName: "",
+      selectedShelterDisasterScale: "",
+      renderedCoordinates: [],
       selectedShelterName: "",
       shelterNames: [],
-      shelterDisasterCoordinates: null,
 
       //map section
-      zoom: 15,
-      center: [12.492442,41.890170],
+      zoom: 5,
+      // center: [106, -6], //for Indonesia
+      center: [0, 0],
       rotation: 0,
+      shelterDisasterCoordinates: [],
 
+      //chart section
+      countVictimInCurrentScope: "0",
+      currentDashboardScope: "Pada seluruh bencana",
+      victimTemplateChart,
+      victimData: {
+        Gender: {
+          title: "Gender",
+          label: ["Laki-laki", "Perempuan"],
+          count: []
+        },
+        Age: {
+          title: "Age",
+          label: ["<10", "10-19", "20-39", "40-59", "60-79", ">79"],
+          count: []
+        },
+        Condition: {
+          title: "Condition",
+          label: ["Hidup", "Meninggal"],
+          count: []
+        }
+      },
+
+      //setup
       mapOptions: {
         attributionControl: false
       },
@@ -203,9 +217,51 @@ export default {
           return obj.DisasterName == val
           }).map(x=>x.Name)
       },
+
+      renderedDashboardData: function () {
+        this.createChart('victim-by-age');
+      }
     },
 
   methods: {
+    btnClickLihatBencana (event) {
+      //filter data to current disaster only
+      var selectedData = this.searchSelectedDisasterData(this.selectedShelterDisasterName)[0]
+      this.selectedShelterDisasterScale = selectedData.Scale
+      this.renderedCoordinates = [{"DisasterID":selectedData.DisasterID, "Latitude":selectedData.Latitude, "Longitude":selectedData.Longitude}]
+      this.countVictimInCurrentScope = this.countVictimInDisasterScope(selectedData.DisasterID)
+      this.renderedDashboardData = this.searchSelectedDashboardData(selectedData.DisasterID)
+      console.log("current rendered data: ", this.renderedDashboardData)
+    },
+
+    btnClickLihatPosko (event) {
+      console.log("button licked")
+    }, 
+
+    searchSelectedDisasterData (selectedShelterDisasterName) {
+      return (
+        this.disastersData.filter(function (disaster) {
+          return disaster.Name == selectedShelterDisasterName
+        })
+      )
+    },
+
+    searchSelectedDashboardData (disasterID) {
+      return (
+        this.dashboardData.filter(function (data) {
+          return data.DisasterID == disasterID
+        })
+      )
+    },
+
+    countVictimInDisasterScope (disasterID) {
+      return (
+        this.dashboardData.filter(function (victimData) {
+          return victimData.DisasterID == disasterID
+        }).length
+      )
+    },
+
     createChart(chartId) {
       const ctx = document.getElementById(chartId);
       
@@ -226,26 +282,45 @@ export default {
       });
     },
 
-    btnClickLihat (event) {
-      console.log("Button CLicked")
+    countAgeData () {
+      var result = [0, 0, 0, 0, 0, 0]
+
+      this.renderedDashboardData.forEach(data => {
+        // bins: "<10", "10-19", "20-39", "40-59", "60-79", ">79"
+        if(data.VictimAge < 10){
+          result[0] += 1
+          }
+        else if(data.VictimAge >= 10 && data.VictimAge < 20){
+          result[1] += 1
+          }
+        else if(data.VictimAge >= 20 && data.VictimAge < 40){
+          result[2] += 1
+          }
+        else if(data.VictimAge >= 40 && data.VictimAge < 60){
+          result[3] += 1
+          }
+        else if(data.VictimAge >= 60 && data.VictimAge < 80){
+          result[4] += 1
+          }
+        else if(data.VictimAge > 79){
+          result[5] += 1
+        }
+      });
+      return result
     },
 
-
     fillData(idx){
-      let label = this.victimData[idx].label
-      let count = this.victimData[idx].count
-      let title = this.victimData[idx].title
-
       var result = this.victimTemplateChart
-      result.data.labels = label
-      result.data.datasets[0].data = count
-      result.data.datasets[0].label = title
-      result.options.title.text = `Victim by ${title}`
-      var dataColor = 'rgba(54,73,93,.5)'
-      // for(let i = 0; i < label.length; i++) { 
-      //   dataColor.push('#8b0000') 
-      //   }
-      result.data.datasets.backgroundColor = dataColor
+      
+      result.data.labels = this.victimData[idx].label
+      result.data.datasets[0].label = this.victimData[idx].title
+      result.options.title.text = `Victim by ${idx}`
+      result.data.datasets.backgroundColor = 'rgba(100,73,93,1)'
+
+      if (idx == "Age") {
+      result.data.datasets[0].data = this.countAgeData()
+      }
+
       return result
     },
   },
@@ -262,16 +337,20 @@ export default {
         this.dashboardData = response[2].data.data
 
         this.shelterDisasterNames = this.disastersData.map(x=>x.Name)
-        this.shelterDisasterCoordinates = this.disastersData.map(x=>[x.Latitude, x.Longitude])
+        this.shelterDisasterCoordinates = this.disastersData.map(x=>({"DisasterID":x.DisasterID, "Latitude":x.Latitude, "Longitude":x.Longitude}))
+        
+        this.renderedDashboardData = this.dashboardData
+        this.renderedCoordinates = this.shelterDisasterCoordinates
+        this.countVictimInCurrentScope = this.dashboardData.length
+
+        // this.createChart('victim-by-gender'); //gaada data gender. Anyway gender gapenting juga kayaknya. ntar liat lagi
+        this.createChart('victim-by-age');
+        // this.createChart('victim-by-condition');
       })
       .catch(error => {
         this.errors.push(error)
         console.log(error)
       })
-
-    this.createChart('victim-by-gender');
-    this.createChart('victim-by-age');
-    this.createChart('victim-by-condition');
   }
 }
 
