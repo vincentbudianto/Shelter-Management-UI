e09de3c4548b52eef75d3be6e5a1a0048d5fc39b
Fatur Rahman <13517056@std.stei.itb.ac.id>
2020-04-08 20:59:09 +0700

    integrate with api

--- src/components/Register/Register.vue
+++ src/components/Register/Register.vue
@@ -4,32 +4,33 @@
             <h4>
                 Register an Account
             </h4>
-            <div>
+            <form @submit="formSubmit" enctype="multipart/form-data">
                 <span>Username</span><br>
-                <input type="text" id="username_placeholder" name="username" value="">
+                <input type="text" id="username_placeholder" name="username" value="" v-model="username">
                 <br>
                 <span class="error-message" id="username_error"></span><br>
                 <span>Email Address</span><br>
-                <input type="text" id="email_placeholder" name="email" value="">
+                <input type="text" id="email_placeholder" name="email" value="" v-model="email">
                 <br>
                 <span class="error-message" id="email_error"></span><br>
                 <span>Phone Number</span><br>
-                <input type="text" id="phone_number_placeholder" name="phone_number" value="">
+                <input type="text" id="phone_number_placeholder" name="phone_number" value="" v-model="phone_number">
                 <span class="error-message" id="phone_error"></span><br>
                 <span>Password</span><br>
-                <input type="password" id="password_placeholder" name="password" value="">
+                <input type="password" id="password_placeholder" name="password" value="" v-model="password">
+                <br>
                 <span class="error-message" id="password_error"></span><br>
                 <span>Confirm Password</span><br>
-                <input type="password" id="cpassword_placeholder" name="confirm_password" value="">
+                <input type="password" id="cpassword_placeholder" name="confirm_password" value="" v-model="confirm_password">
                 <br>
                 <span class="error-message" id="cpassword_error"></span><br>
                 <span>Profile Picture</span><br>
                 <div class="upload-btn-wrapper">
-                    <input type="file" id="real-upload-button" name="profile-picture"/>
+                    <input type="file" id="real-upload-button" name="profile-picture" ref="file" v-on:change="handleFileUpload()"/>
                 </div>
                 <br><br>
                 <input class="button" id="submit_button" type="submit" value="Register">
-            </div> 
+            </form> 
 
             <p>Already have account? <a :href="'/#/login'">Login here</a> </p>
 
@@ -123,4 +124,83 @@ body {
 </style>
 
 <script>
+import axios from 'axios';
+    export default {
+
+        mounted() {
+
+            console.log('Component mounted.')
+
+        },
+
+        data() {
+
+            return {
+
+              username: '',
+
+              email: '',
+
+              phone_number: '',
+
+              password: '',
+
+              file: '',
+
+              output: ''
+
+            };
+
+        },
+
+        methods: {
+
+            formSubmit(e) {
+
+                e.preventDefault();
+
+                let formData = new FormData();
+
+                formData.append('username', this.username);
+                formData.append('email', this.email);
+                formData.append('phone_number', this.phone_number);
+                formData.append('password', this.password);
+                formData.append('photo', this.file);
+
+                let currentObj = this;
+
+                if (this.password == this.confirm_password) {
+                    document.getElementById("password_error").innerHTML = "";
+                    document.getElementById("cpassword_error").innerHTML = "";
+                    axios.post('http://localhost:3000/register', formData, 
+                    {
+                        headers: {
+                            'Content-Type': 'multipart/form-data'
+                        }
+                    })
+
+                    .then(function (response) {
+
+                        currentObj.output = response.data;
+
+                    })
+
+                    .catch(function (error) {
+
+                        currentObj.output = error;
+
+                    });
+                } else {
+                    document.getElementById("password_error").innerHTML = "Password does not match";
+                    document.getElementById("cpassword_error").innerHTML = "Password does not match";
+                }
+
+            }, handleFileUpload(){
+                this.file = this.$refs.file.files[0];
+            }
+
+        }
+
+    }
+
 </script>
