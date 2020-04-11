1b8c9dbe813f2f2979a1dcc8e0470b4a02b42a6a
Fatur Rahman <13517056@std.stei.itb.ac.id>
2020-04-11 20:33:52 +0700

    can login, accountid & type in cookies

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,18 +1,24 @@
 <template>
 <div class="container login-container d-flex justify-content-center">
     <div class="col-md-6 login-form-1">
-        <h3>Login Here</h3>
+        <h3 id="title">Login</h3>
+        <form @submit="formSubmit">
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Username" value="" name="username" v-model="username"/>
+            </div>
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Password" value="" name="password" v-model="password"/>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btnSubmit" value="Login" />
+            </div>
+        </form>
+        <span class="error-message" id="login_failed"></span><br>
         <div class="form-group">
-            <input type="text" class="form-control" placeholder="Your Email *" value="" />
+            <a href="#" class="btnForgetPwd">Lupa Password?</a>
         </div>
         <div class="form-group">
-            <input type="password" class="form-control" placeholder="Your Password *" value="" />
-        </div>
-        <div class="form-group">
-            <input type="submit" class="btnSubmit" value="Login" />
-        </div>
-        <div class="form-group">
-            <a href="#" class="btnForgetPwd">Forget Password?</a>
+            <a href="#/register" class="btnForgetPwd">Daftar Akun</a>
         </div>
     </div>
 </div>
@@ -63,8 +69,84 @@
     text-decoration:none;
     color:#fff;
 }
+.error-message{
+    color: white!important;
+    font-size: 0.6em!important;
+}
 </style>
 
 <script>
+import axios from 'axios';
+    export default {
+
+        mounted() {
+
+            console.log('Component mounted.')
+
+        },
+
+        data() {
+
+            return {
+
+              AccountId: '',
+
+              Username: '',
+
+              Type: '',
+
+              NIK: '',
+
+              NoKK: '',
+
+              Name: '',
+
+              Age: '',
+
+              Photo: '',
+
+              CurrentShelterId: ''
+
+            };
+
+        },
+
+        methods: {
+
+            formSubmit(e) {
+
+                e.preventDefault();
+
+                let currentObj = this;
+                axios.post('http://localhost:3000/login',  
+                {
+                    username: this.username,
+                    password: this.password
+                })
+
+                .then(function (response) {
+
+                    currentObj.data = response.data.data;
+                    if (currentObj.data.length > 0) {
+                        currentObj.$cookies.set("AccountId", currentObj.data[0].AccountId, "43200")
+                        currentObj.$cookies.set("Type", currentObj.data[0].Type, "43200s")
+                        currentObj.$router.push('/');
+                    } else {
+                        document.getElementById("login_failed").innerHTML = "Login gagal"
+                    }
+
+                })
+
+                .catch(function (error) {
+
+                    currentObj.output = error;
+
+                });
+
+            }
+
+        }
+
+    }
 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueCookies from 'vue-cookies'
 import App from './App'
 import router from './router'
-import ChartjsPlugin from './chartjs_plugin.js';
-
+import ChartjsPlugin from './chartjs_plugin.js'
 
 Vue.config.productionTip = false
 Vue.use(ChartjsPlugin)
+Vue.use(VueCookies)
 
 /* eslint-disable no-new */
 new Vue({
