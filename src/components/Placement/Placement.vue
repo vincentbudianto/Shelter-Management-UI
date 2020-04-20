894da87701cd196ff5376cf915aed1e23f8e8da4
Fatur Rahman <13517056@std.stei.itb.ac.id>
2020-04-20 19:32:49 +0700

    add admin and staff validation to placement

--- src/components/Placement/Placement.vue
+++ src/components/Placement/Placement.vue
@@ -120,10 +120,33 @@ export default {
       .catch(e => {
         this.errors.push(e)
       })
+    }, validateUser(){
+        var aid = this.$cookies.get('AccountID');
+        let currentObj = this;
+        axios.get('http://localhost:3000/check/staff?id=' + aid)
+        .then(response => {
+            // JSON responses are automatically parsed.
+            if(response.data.data.isStaff == false){
+                axios.get('http://localhost:3000/check/admin?id=' + aid)
+                .then(response => {
+                    // JSON responses are automatically parsed.
+                    if(response.data.data.isAdmin == false){
+                        currentObj.$router.push('/login');
+                    }
+                })
+                .catch(e => {
+                    this.errors.push(e)
+                })
+            }
+        })
+        .catch(e => {
+            this.errors.push(e)
+        })
     }
   },
   beforeMount(){
-    this.get_placement()
+    this.validateUser();
+    this.get_placement();
   },
 }
 </script>
